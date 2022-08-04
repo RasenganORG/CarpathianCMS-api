824f04679ee6067f7b8d3251060fd7144d9c0deb
Edward484 <selea.eduard@gmail.com>
2022-08-04 13:10:54 +0300

    finished CRUD operations for page controller

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,6 +1,6 @@
 import firebase from '../db.js'
 import fetch from "node-fetch";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import {collection, query, where, getDocs} from "firebase/firestore";
 
 
 const firestore = firebase.firestore()
@@ -19,7 +19,11 @@ const {
 export const addNewPage = async (req, res) => {
     try {
         const data = req.body;
-        const pageRef = await firestore.collection("pages").doc();
+        let siteId = data.siteId ?? '123';
+        const pageRef = await firestore.collection("sites")
+            .doc(siteId)
+            .collection('pages')
+            .doc();
         await pageRef.create(data);
         res.status(200).send("Page created successfully");
     } catch (error) {
@@ -30,8 +34,12 @@ export const addNewPage = async (req, res) => {
 export const getPage = async (req, res) => {
     try {
         const data = req.body;
-        const id = data.id;
-        const pageRef = await firestore.collection('pages').doc(id);
+        const pageId = data.pageId;
+        const siteId = data.siteId;
+        const pageRef = await firestore.collection('sites')
+            .doc(siteId)
+            .collection('pages')
+            .doc(pageId);
         const pageData = await pageRef.get();
         if (!pageData.exists) {
             res.status(404).send('Page with the given ID not found');
@@ -44,20 +52,59 @@ export const getPage = async (req, res) => {
     }
 }
 
-export const getPagesBySiteId = async (req,res) => {
+export const getPagesBySiteId = async (req, res) => {
     try {
         let responseArray = []
         const data = req.body;
-        const id = data.id;
-        const pagesRef = firestore.collection('pages');
-        const querySnapshot = await pagesRef.where('siteId', '==', id).get()
-        querySnapshot.forEach((doc) => {
-            console.log(doc.id, " => ", doc.data());
-            responseArray.push(doc.data())
-        });
-        res.send(responseArray)
+        const id = data.siteId;
+        const pagesRef = firestore.collection('sites').doc(id).collection('pages');
+        const querySnapshot = await pagesRef.get()
+        if (querySnapshot.empty) {
+            res.status(404).send("Page not found")
+        } else {
+            querySnapshot.forEach((doc) => {
+                responseArray.push({
+                    id: doc.id,
+                    data: doc.data()
+                })
+            });
+            res.send(responseArray)
+        }
 
     } catch (error) {
         res.status(400).send(error.message);
     }
 }
+
+
+export const updatePage = async (req, res) => {
+    try {
+        const siteId = req.body.siteId
+        const pageId = req.body.pageId
+        const data = req.body.data
+        const pageRef = await firestore.collection('sites')
+            .doc(siteId)
+            .collection('pages')
+            .doc(pageId)
+        await pageRef.update(data);
+        res.send('User record updated successfully');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+};
+
+export const deletePage  = async (req, res) => {
+    try {
+        const siteId = req.body.siteId
+        const pageId = req.body.pageId
+        await firestore.collection('sites')
+            .doc(siteId)
+            .collection('pages')
+            .doc(pageId)
+            .delete();
+        res.send('Page deleted successfully');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+};
+
--- routes/pages.js
+++ routes/pages.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import {addNewPage, getPage, getPagesBySiteId} from "../controllers/pages.js";
+import {addNewPage, deletePage, getPage, getPagesBySiteId, updatePage} from "../controllers/pages.js";
 const router = express.Router();
 
 router.post('/add-new-page',addNewPage);
 router.get('/get-page-by-id',getPage);
 router.get('/get-pages-by-siteid',getPagesBySiteId);
+router.get('/update-page',updatePage);
+router.delete('/delete-page',deletePage);
 
 export default router
