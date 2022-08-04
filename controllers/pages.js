412ae090d7e6118db16b6dc92606a76ad34b2387
Edward484 <selea.eduard@gmail.com>
2022-08-04 12:23:17 +0300

    worked on pages controller

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,5 +1,8 @@
 import firebase from '../db.js'
 import fetch from "node-fetch";
+import { collection, query, where, getDocs } from "firebase/firestore";
+
+
 const firestore = firebase.firestore()
 //import Page from '../controllers/pages'
 
@@ -16,10 +19,45 @@ const {
 export const addNewPage = async (req, res) => {
     try {
         const data = req.body;
-        console.log(data)
-        res.status(200).send("ok")
+        const pageRef = await firestore.collection("pages").doc();
+        await pageRef.create(data);
+        res.status(200).send("Page created successfully");
+    } catch (error) {
+        res.status(400).send(error.message);
     }
-    catch (error){
+}
+
+export const getPage = async (req, res) => {
+    try {
+        const data = req.body;
+        const id = data.id;
+        const pageRef = await firestore.collection('pages').doc(id);
+        const pageData = await pageRef.get();
+        if (!pageData.exists) {
+            res.status(404).send('Page with the given ID not found');
+        } else {
+            res.send(pageData.data());
+        }
+
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+export const getPagesBySiteId = async (req,res) => {
+    try {
+        let responseArray = []
+        const data = req.body;
+        const id = data.id;
+        const pagesRef = firestore.collection('pages');
+        const querySnapshot = await pagesRef.where('siteId', '==', id).get()
+        querySnapshot.forEach((doc) => {
+            console.log(doc.id, " => ", doc.data());
+            responseArray.push(doc.data())
+        });
+        res.send(responseArray)
+
+    } catch (error) {
         res.status(400).send(error.message);
     }
 }
--- routes/pages.js
+++ routes/pages.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import {addNewPage} from "../controllers/pages.js";
+import {addNewPage, getPage, getPagesBySiteId} from "../controllers/pages.js";
 const router = express.Router();
 
 router.post('/add-new-page',addNewPage);
+router.get('/get-page-by-id',getPage);
+router.get('/get-pages-by-siteid',getPagesBySiteId);
 
 export default router
