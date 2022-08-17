80ec8e3539809488b10c75b9c9ad979528070cb7
Edward484 <selea.eduard@gmail.com>
2022-08-17 17:33:50 +0300

    changed updateTag from patch to put

--- routes/pages.js
+++ routes/pages.js
@@ -13,7 +13,7 @@ router.post('/addNewPage/:siteId',addNewPage);
 router.get('/getPage/:siteId/:pageId',getPage);
 router.get('/getPages/:siteId',getPagesBySiteId);
 router.get('/getNavbar/:siteId',getNavbarBySiteId);
-router.patch('/updatePage/:siteId/:pageId',updatePage);
+router.put('/updatePage/:siteId/:pageId',updatePage);
 router.delete('/deletePage/:siteId/:pageId',deletePage);
 
 export default router
